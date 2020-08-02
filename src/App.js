ed44e78780f4e117c44e3928261525da20077f03
velmor99 <volgagaz24100@gmail.com>
2020-08-02 22:10:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+		filter: ''
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleAddToContacts = (e) => {
+		const contact = {
+			id: uuidv4(),
+			name: this.state.name,
+			number: this.state.number
+		};
+
+		if (contact.name === '' || contact.number === '') {
+			alert('One of the fields is not filled');
+		} else if (
+			this.state.contacts.find((contact) => contact.name.toLowerCase() === this.state.name.toLowerCase())
+		) {
+			alert(`${this.state.name} is already in contacts`);
+		} else {
+			this.setState((prevState) => ({
+				contacts: [ ...prevState.contacts, contact ]
+			}));
+		}
+	};
+
+	changeFilter = (filter) => {
+		this.setState({ filter });
+	};
+
+	handleFilter = (e) => {
+		const { contacts, filter } = this.state;
+		return contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  };
+  
+  removeContact = id => {
+    this.setState(prevState => {
+			return {
+				contacts: prevState.contacts.filter(contact => contact.id !== id)
+			}
+		})
+  }
+
+	render() {
+		const filteredArr = this.handleFilter();
+		return (
+			<div>
+				<h2>Phonebook</h2>
+				<ContactForm
+					preventSubmit={this.handleSubmit}
+					nameChange={this.handleChange}
+					numberChange={this.handleChange}
+					clickEvent={this.handleAddToContacts}
+				/>
+				<h2>Contacts</h2>
+				<Filter value={this.state.filter} onChangeFilter={this.changeFilter} />
+        <ContactList 
+        click={this.removeContact}
+        filteredArr={filteredArr} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styleContactForm.module.css'
+
+export default function ContactForm ({preventSubmit, nameChange, numberChange, clickEvent}) {
+    return (
+        <form className={styles.form} onSubmit={preventSubmit}>
+          <label>
+            Name
+            <input className={styles.inputName} name="name" type="text" onChange={nameChange}></input>
+            <br />
+            Number
+            <input className={styles.inputNumber} name="number" type="text" onChange={numberChange}></input>
+          </label>
+          <br />
+          <button className={styles.button, styles.submit} name="name" onClick={clickEvent} type="submit">Add to contacts</button>
+        </form>
+    )
+}
