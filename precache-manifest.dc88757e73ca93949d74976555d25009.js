7ce2911f3c1db2abb769d96dd88a2d08975b6753
velmor99 <volgagaz24100@gmail.com>
2020-08-02 22:12:26 +0300

    Updates

---
+++ precache-manifest.dc88757e73ca93949d74976555d25009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d61eb895a7adec8985c2ae70d03bd0",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "ad6dc231d0d25989b4e7",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.075eab6b.chunk.css"
+  },
+  {
+    "revision": "95b4ee7ef36e3f94ad67",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a699c51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a699c51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6dc231d0d25989b4e7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9bf76a26.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
